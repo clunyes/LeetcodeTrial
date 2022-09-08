1161852cbf770b0ac764822247940605d3b38b9f
赵康 <kang.zhao@cnjiang.com>
2022-09-08 14:31:06 +0800

    feat:9.8

---
+++ src/com/demo/leetcode/Leetcode1046.java
@@ -0,0 +1,33 @@
+package com.demo.leetcode;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class Leetcode1046 {
+
+    public static void main(String[] args) {
+//        int[] arr = new int[]{2, 7, 4, 1, 8, 1};
+        int[] arr = new int[]{100, 1, 1};
+        System.out.println(lastStoneWeight(arr));
+    }
+
+    public static int lastStoneWeight(int[] stones) {
+        if (stones.length == 1) {
+            return stones[0];
+        }
+        if (stones.length == 0) {
+            return 0;
+        }
+        Arrays.sort(stones);
+        int r1 = stones[stones.length - 1];
+        int r2 = stones[stones.length - 2];
+        int[] next;
+        if (r1 != r2) {
+            next = Arrays.copyOfRange(stones, 0, stones.length - 1);
+            next[stones.length - 2] = Math.abs(r1 - r2);
+        } else {
+            next = Arrays.copyOfRange(stones, 0, stones.length - 2);
+        }
+        return lastStoneWeight(next);
+    }
+}
---
+++ src/com/demo/leetcode/onethousand/Leetcode692.java
@@ -0,0 +1,86 @@
+package com.demo.leetcode.onethousand;
+
+import javafx.util.Pair;
+
+import java.lang.annotation.Inherited;
+import java.util.*;
+
+public class Leetcode692 {
+
+    public static void main(String[] args) {
+//        String[] words = new String[]{"i", "love", "leetcode", "i", "love", "coding"};
+        String[] words = new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
+        topKFrequent(words, 4);
+    }
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> data = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            data.put(word, data.getOrDefault(word, 0) + 1);
+        }
+        ArrayList<Word> list = new ArrayList();
+        for (String key : data.keySet()) {
+            list.add(new Word(key, data.get(key)));
+        }
+        Collections.sort(list);
+        List<String> result = new ArrayList<>();
+        for (int i = list.size() - 1; i > list.size() - 1 - k; i--) {
+            result.add(list.get(i).word);
+        }
+        return result;
+    }
+
+    /**
+     * 小根堆 大根堆 都可以用PriorityQueue实现
+     * @param words
+     * @param k
+     * @return
+     */
+//    public List<String> topKFrequent(String[] words, int k) {
+//        Map<String, Integer> cnt = new HashMap<String, Integer>();
+//        for (String word : words) {
+//            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+//        }
+//        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+//            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+//                return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+//            }
+//        });
+//        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+//            pq.offer(entry);
+//            if (pq.size() > k) {
+//                pq.poll();
+//            }
+//        }
+//        List<String> ret = new ArrayList<String>();
+//        while (!pq.isEmpty()) {
+//            ret.add(pq.poll().getKey());
+//        }
+//        Collections.reverse(ret);
+//        return ret;
+//    }
+
+    static class Word implements Comparable<Word> {
+        String word;
+        int num;
+
+        Word(String word, int num) {
+            this.word = word;
+            this.num = num;
+        }
+
+
+        @Override
+        public int compareTo(Word w) {
+            if (this.num > w.num) {
+                return 1;
+            } else if (this.num == w.num) {
+                return w.word.compareTo(this.word);
+            } else {
+                return -1;
+            }
+        }
+    }
+
+}
