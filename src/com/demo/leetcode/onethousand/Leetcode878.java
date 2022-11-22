cf5f36435298d33324ede3348f8d657c395cbbe7
kang.zhao <kang.zhao@ecarx.com.cn>
2022-11-22 12:03:01 +0800

    feat:11.22

---
+++ src/com/demo/leetcode/onethousand/Leetcode878.java
@@ -0,0 +1,12 @@
+package com.demo.leetcode.onethousand;
+
+public class Leetcode878 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int nthMagicalNumber(int n, int a, int b) {
+
+    }
+}
