fc73556bdeb03af6bf2865fdcd65d0d22912c05b
kang.zhao <kang.zhao@ecarx.com.cn>
2023-07-05 15:48:40 +0800

    feat:0705

---
+++ src/com/demo/demo/finalstaticDemo/StaticA.java
@@ -0,0 +1,17 @@
+package com.demo.demo.finalstaticDemo;
+
+public class StaticA {
+    private int x;        // 实例变量
+    public static int y;  // 静态变量
+
+    /**
+     * 存在继承的情况下，初始化顺序为：
+     *
+     * 父类（静态变量、静态语句块）
+     * 子类（静态变量、静态语句块）
+     * 父类（实例变量、普通语句块）
+     * 父类（构造函数）
+     * 子类（实例变量、普通语句块）
+     * 子类（构造函数）
+     */
+}
---
+++ src/com/demo/demo/keyword/Son.java
@@ -0,0 +1,12 @@
+package com.demo.demo.keyword;
+
+public class Son extends Father {
+
+    void eat() {
+        System.out.println("son eat");
+    }
+
+    void speak() {
+        System.out.println("son speak");
+    }
+}
