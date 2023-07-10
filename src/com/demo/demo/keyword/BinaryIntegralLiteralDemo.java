6ecd1e383ffca74c36f72aaa40029d7c5bbef974
kang.zhao <kang.zhao@ecarx.com.cn>
2023-07-10 20:21:53 +0800

    feat:0710

---
+++ src/com/demo/demo/keyword/BinaryIntegralLiteralDemo.java
@@ -0,0 +1,21 @@
+package com.demo.demo.keyword;
+
+public class BinaryIntegralLiteralDemo {
+    public static void main(String[] args) {
+        System.out.println(0b010101);
+        System.out.println(0B010101);
+        System.out.println(0x15A);
+        System.out.println(0X15A);
+        System.out.println(077);
+        System.out.println(5_000);
+        /**
+         * 输出结果
+         * 21
+         * 21
+         * 346
+         * 346
+         * 63
+         * 5000
+         */
+    }
+}
---
+++ src/com/demo/demo/keyword/SignalDemo.java
@@ -0,0 +1,15 @@
+package com.demo.demo.keyword;
+
+public class SignalDemo {
+
+    public static void main(String[] args) {
+        //报错
+//        if((23!=23) & (100/0==0)){
+        if((23!=23) && (100/0==0)){
+            System.out.println("运算没有问题。");
+        }else{
+            System.out.println("没有报错");
+        }
+    }
+
+}
