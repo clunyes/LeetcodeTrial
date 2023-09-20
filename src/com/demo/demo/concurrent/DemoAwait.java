b187407ef84d13e0655661a5d0474db21251303c
kang.zhao <kang.zhao@ecarx.com.cn>
2023-09-20 09:49:10 +0800

    feat:20230920 1

---
+++ src/com/demo/demo/concurrent/DemoAwait.java
@@ -0,0 +1,13 @@
+package com.demo.demo.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DemoAwait {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        AwaitSignalExample example = new AwaitSignalExample();
+        executorService.execute(() -> example.after());
+        executorService.execute(() -> example.before());
+    }
+}
