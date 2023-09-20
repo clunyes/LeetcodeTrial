0cbd40820a72824431be38de58080c58a82d1b24
kang.zhao <kang.zhao@ecarx.com.cn>
2023-09-20 19:42:06 +0800

    feat:20230920

---
+++ src/com/demo/demo/concurrent/unsafe/UnsafeTest.java
@@ -0,0 +1,25 @@
+package com.demo.demo.concurrent.unsafe;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UnsafeTest {
+    public static void main(String[] args) throws InterruptedException {
+        final int threadSize = 1000;
+        ThreadUnsafeExample example = new ThreadUnsafeExample();
+        final CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadSize; i++) {
+            executorService.execute(() -> {
+                example.add();
+                countDownLatch.countDown();
+            });
+        }
+        System.out.println("await");
+        countDownLatch.await();
+        System.out.println("shutdown");
+        executorService.shutdown();
+        System.out.println(example.get());
+    }
+}
