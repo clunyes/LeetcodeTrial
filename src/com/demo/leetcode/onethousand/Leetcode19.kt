528073d370ab7a95960cda0d9e2e3159c5e9e063
kang.zhao <kang.zhao@ecarx.com.cn>
2024-01-22 13:53:16 +0800

    feat:fix

--- src/com/demo/leetcode/onethousand/Leetcode19.kt
+++ src/com/demo/leetcode/onethousand/Leetcode19.kt
@@ -5,11 +5,11 @@ import com.demo.leetcode.datastruct.ListNode.printList
 
 fun main() {
     val node = ListNode(1)
-    node.next = ListNode(2)
-    node.next.next = ListNode(3)
-    node.next.next.next = ListNode(4)
-    node.next.next.next.next = ListNode(5)
-    printList(removeNthFromEnd(node, 2))
+//    node.next = ListNode(2)
+//    node.next.next = ListNode(3)
+//    node.next.next.next = ListNode(4)
+//    node.next.next.next.next = ListNode(5)
+    printList(removeNthFromEnd(node, 1))
 }
 
 fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
@@ -25,5 +25,5 @@ fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         start = start.next
     }
     start.next = start.next.next
-    return head
+    return pre.next
 }
